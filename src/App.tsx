4ee4bda7c76d8ec3ef508f67bbed8717aff41241
timdecillis <timdecillis@gmail.com>
2024-10-21 10:43:59 -0700

    added all chip sizes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import createChip from "./lib/chipCreator";
 import Container from "./lib/Container";
 
 
-const displayComponents = [createChip("primary", "medium"), createChip("secondary", "medium"), createChip("terniary", "medium"), createChip("secondary", 'small')];
+const displayComponents = [createChip("primary", "medium"), createChip("secondary", "medium"), createChip("terniary", "medium"), createChip("secondary", 'small'), createChip("secondary", 'medium'), createChip("secondary", 'large')];
 
 function App() {
   return (
