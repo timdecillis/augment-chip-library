0e998e6744937b61906948ec1d2993d33430b95a
timdecillis <timdecillis@gmail.com>
2024-10-22 14:07:31 -0700

    updated container layout

--- src/lib/Container/Container.tsx
+++ src/lib/Container/Container.tsx
@@ -1,9 +1,15 @@
-import './globals.css'
+import "./globals.css";
 
 function Container({ components }: any) {
-  return <div className='container' >{
-    components.map((Comp: any, index: number) => <Comp key={index} />)
-    }</div>;
+  return (
+    <div className="container">
+      <div>
+        {components.map((Comp: any, index: number) => (
+          <Comp key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Container;
