280b243897c547b50627645bd5a28a4a1b54c0d5
timdecillis <timdecillis@gmail.com>
2024-10-23 17:30:35 -0700

    added label to display chips

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Container from "./lib/Container/Container";
 const icon = <i className="fa-solid fa-music"></i>
 const addOn = <i className="fa-solid fa-chevron-down"></i>
 
-const displayComponents = [createChip("primary", "medium", icon, addOn), createChip("secondary", "medium", icon, addOn), createChip("terniary", "medium", icon, addOn), createChip("secondary", 'small', icon, addOn), createChip("secondary", 'medium', icon, addOn), createChip("secondary", 'large', icon, addOn)];
+const displayComponents = [createChip("label", "primary", "medium", icon, addOn), createChip("label", "secondary", "medium", icon, addOn), createChip("label", "terniary", "medium", icon, addOn), createChip("label", "secondary", 'small', icon, addOn), createChip("label", "secondary", 'medium', icon, addOn), createChip("label", "secondary", 'large', icon, addOn)];
 
 function App() {
   return (
