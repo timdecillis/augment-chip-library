76236c347872235a6d3abb3113b3bb2e48daf14e
timdecillis <timdecillis@gmail.com>
2024-10-23 17:29:41 -0700

    added dynamic label

--- src/utils/chipCreator.tsx
+++ src/utils/chipCreator.tsx
@@ -1,12 +1,12 @@
 import AugmentChip from "../lib/AugmentChip/AugmentChip";
 
-const createChip = (type: string, size: string, startIcon: any, endAddOn: any, isAnimating: boolean = true) => (props: any) =>
+const createChip = (label: string, type: string, size: string, startIcon: any, endAddOn: any, isAnimating: boolean = true) => (props: any) =>
   (
     <AugmentChip
       type={type}
       size={size}
       startIcon={startIcon}
-      label="Label"
+      label={label}
       isAnimating={isAnimating}
       endAddOn={endAddOn}
       {...props}
