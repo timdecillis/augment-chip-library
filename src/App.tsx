6c09357203bed1f2f7a71eb7410ec9690561792a
timdecillis <timdecillis@gmail.com>
2024-10-24 23:29:07 -0700

    created chipArray creator

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,34 @@ import "./App.css";
 import createChip from "./utils/chipCreator";
 import Container from "./lib/Container/Container";
 
-const icon = <i className="fa-solid fa-music"></i>
-const addOn = <i className="fa-solid fa-chevron-down"></i>
+const icon = <i className="fa-solid fa-music"></i>;
+const addOn = <i className="fa-solid fa-chevron-down"></i>;
 
-const displayComponents = [createChip("label", "primary", "medium", icon, addOn, false), createChip("label", "secondary", "medium", icon, addOn), createChip("label", "terniary", "medium", icon, addOn), createChip("label", "secondary", 'small', icon, addOn), createChip("label", "secondary", 'medium', icon, addOn), createChip("this is a really long label hey hey hey this is a long label", "secondary", 'large', icon, addOn)];
+const chipArrayCreator = (
+  arrays: [string, string, string, JSX.Element, JSX.Element, boolean?][]
+) => {
+  arrays.map((array) => {
+    return createChip(...array);
+  });
+};
+
+const chipParameters = [
+  ["label", "primary", "medium", icon, addOn, false],
+  ["label", "secondary", "medium", icon, addOn, false],
+  ["label", "terniary", "medium", icon, addOn, false],
+  ["label", "secondary", "small", icon, addOn, false],
+  ["label", "secondary", "medium", icon, addOn, false],
+  [
+    "this is a really long label hey hey hey this is a long label",
+    "secondary",
+    "large",
+    icon,
+    addOn,
+    false,
+  ],
+];
+
+const displayComponents = chipArrayCreator(chipParameters);
 
 function App() {
   return (
