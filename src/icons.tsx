190386d6d608fab33bf2782e8a7ecca67b9a7bd7
timdecillis <timdecillis@gmail.com>
2024-10-25 10:52:23 -0700

    added exports to icons

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,2 +1,2 @@
-const icon = <i className="fa-solid fa-music"></i>;
-const addOn = <i className="fa-solid fa-chevron-down"></i>;
+export const icon = <i className="fa-solid fa-music"></i>;
+export const addOn = <i className="fa-solid fa-chevron-down"></i>;
