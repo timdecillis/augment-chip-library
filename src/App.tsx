28b788e49873feb509cbd9043232d30371e5f43d
timdecillis <timdecillis@gmail.com>
2024-10-25 11:02:18 -0700

    updated chipCreator util

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import "./App.css";
 import Container from "./lib/Container/Container";
-import { icon, addOn } from "./icons";
-import { createChipArray } from "./utils/chipCreator";
+import displayComponents from "./utils/chipCreator";
 
-const chipArguments = [
-  ["label", "primary", "medium", icon, addOn, false],
-  ["label", "secondary", "medium", icon, addOn, false],
-  ["label", "terniary", "medium", icon, addOn, false],
-  ["label", "secondary", "small", icon, addOn, false],
-  ["label", "secondary", "medium", icon, addOn, false],
-  [
-    "this is a really long label hey hey hey this is a long label",
-    "secondary",
-    "large",
-    icon,
-    addOn,
-    false,
-  ],
-];
 
-const displayComponents = createChipArray(chipArguments);
 
 function App() {
   return (
--- src/utils/chipCreator.tsx
+++ src/utils/chipCreator.tsx
@@ -1,23 +1,49 @@
 import AugmentChip from "../lib/AugmentChip/AugmentChip";
+import { icon, addOn } from "../icons";
 
-export const createChip = (label: string, type: string, size: string, startIcon: any, endAddOn: any, isAnimating: boolean = true) => (props: any) =>
+const createChip =
   (
-    <AugmentChip
-      type={type}
-      size={size}
-      startIcon={startIcon}
-      label={label}
-      isAnimating={isAnimating}
-      endAddOn={endAddOn}
-      {...props}
-    />
-  );
+    label: string,
+    type: string,
+    size: string,
+    startIcon: any,
+    endAddOn: any,
+    isAnimating: boolean = true
+  ) =>
+  (props: any) =>
+    (
+      <AugmentChip
+        type={type}
+        size={size}
+        startIcon={startIcon}
+        label={label}
+        isAnimating={isAnimating}
+        endAddOn={endAddOn}
+        {...props}
+      />
+    );
 
-  export const createChipArray = (
-    arrays: []
-  ) => {
-    arrays.map((array) => {
-      return createChip(...array);
-    });
-  };
+const createChipArray = (arrays: []) => {
+  arrays.map((array) => {
+    return createChip(...array);
+  });
+};
 
+const chipArguments = [
+  ["label", "primary", "medium", icon, addOn, false],
+  ["label", "secondary", "medium", icon, addOn, false],
+  ["label", "terniary", "medium", icon, addOn, false],
+  ["label", "secondary", "small", icon, addOn, false],
+  ["label", "secondary", "medium", icon, addOn, false],
+  [
+    "this is a really long label hey hey hey this is a long label",
+    "secondary",
+    "large",
+    icon,
+    addOn,
+    false,
+  ],
+];
+
+const displayComponents = createChipArray(chipArguments);
+export default displayComponents;
