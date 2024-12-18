beeeaf8cbd04f84d4ca432ddcfccc902610b7445
Tim DeCillis <timdecillis@gmail.com>
2024-12-18 11:47:21 -0800

    added copy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import "./App.css";
 import Container from "./lib/Container/Container";
 import displayComponents from "./utils/chipCreator";
 
-
-
 function App() {
   return (
     <div className="App">
       <h1>Augment Chip Library</h1>
+      <h2>Augment Chip Options</h2>
       <Container components={displayComponents} />
     </div>
   );
